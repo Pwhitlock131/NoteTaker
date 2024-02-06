9c810ca7e6b91f2a63b7649df39af942f5a5e49e
Pwhitlock <pwhitlock131@gmail.com>
2024-02-06 14:49:52 -0600

    began connecting routes in htmlRoutes and apiRoutes files.

---
+++ routes/apiRoutes.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const fs = require('fs');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+const fileName = require('../db/db.json');
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
