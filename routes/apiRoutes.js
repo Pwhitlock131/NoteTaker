c6794971bfd53f51042b0b2a8a206f448a88f5ee
Pwhitlock <pwhitlock131@gmail.com>
2024-02-06 16:36:24 -0600

    Added post route to apiRoutes.js, and installed uuid which was missing from the package.json

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,27 @@
 const router = require('express').Router();
 const fs = require('fs');
 const fileName = require('../db/db.json');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 
 
 router.get('/notes', (req, res) => {
-    const data = fs.readFileSync(fileName, 'utf8');
+    const data = fs.readFileSync('db/db.json', 'utf8');
     const parsedData = JSON.parse(data);
     res.json(parsedData);
 });
 
 router.post('/notes', (req, res) => {
-    res.json('success');
+    const { title, text } = req.body;
+    const payload = {
+        title,
+        text,
+        id: uuidv4()
+    };
+    console.log(payload);
+    const newData = [...fileName, payload];
+    const data = fs.writeFileSync('db/db.json', JSON.stringify(newData), (err) => console.log(err));
+    res.json(data);
 });
-
-
-
-
-
-
-
-
 
 module.exports = router;
